d7b0f72bb5ff77327d2f6bea137b29bc7ffa808d
Travis Lawrence <travis.lawrence12@gmail.com>
2021-12-12 08:05:58 -0800

    rendering now

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,27 @@
 import NetworkGraph from "../NetworkGraph";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+
+const SNetworkContainer = styled.div``;
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch(`${process.env.PUBLIC_URL}/data_network.json`)
+        .then((d) => d.json())
+        .then((values) => {
+          setData(values);
+        });
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <NetworkGraph />
-    </div>
+    <SNetworkContainer>
+      {data && <NetworkGraph data={data} />}
+    </SNetworkContainer>
   );
 }
 
--- src/components/NetworkGraph/index.js
+++ src/components/NetworkGraph/index.js
@@ -1,59 +1,124 @@
 import * as d3 from "d3";
-import { useEffect, useRef } from "react";
-import styled from "styled-components";
+import { useCallback, useEffect, useRef } from "react";
 
-const SNetworkContainer = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-export const mainWidth = 750;
-export const mainHeight = 400;
-export const margin = { top: 20, right: 30, bottom: 30, left: 30 };
+export const mainWidth = 600;
+export const mainHeight = 600;
+export const margin = { top: 0, right: 0, bottom: 0, left: 0 };
 
 const width = mainWidth - margin.left - margin.right;
 const height = mainHeight - margin.top - margin.bottom;
-const data = fetch(`${process.env.PUBLIC_URL}/data_network.json`)
-  .then((d) => d.json())
-  .then((values) => {
-    console.log(values);
-  });
 
-function NetworkGraph() {
+const CIRCLE_BASE_RADIUS = 20;
+const CIRCLE_RADIUS_MULTIPLIER = 3;
+
+function NetworkGraph({ data }) {
   const graphRef = useRef();
-  // initialize graph
+
   useEffect(() => {
-    // create viewport
     const main = d3
       .select(graphRef.current)
       .attr("width", mainWidth)
       .attr("height", mainHeight)
+      .style("background", "#eee")
       .append("g")
       .attr("class", "main")
       .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
     main.append("g").attr("class", "lines");
-
-    main
-      .append("g")
-      .attr("class", "x-axis")
-      .attr("transform", `translate(0, ${height})`);
-
-    // create y-axis
-    main.append("g").attr("class", "y-axis");
+    main.append("g").attr("class", "nodes");
   }, []);
 
-  // useEffect(() => {
-  //   draw();
-  // }, [data, draw]);
+  const draw = useCallback(() => {
+    const svg = d3.select(graphRef.current);
+    d3.forceSimulation(data.nodes)
+      .force(
+        "link",
+        d3
+          .forceLink()
+          .id(function (d) {
+            return d.id;
+          })
+          .links(data.links)
+      )
+      .force("charge", d3.forceManyBody().strength(-1000))
+      .force("center", d3.forceCenter(width / 2, height / 2))
+      .on("end", ticked);
+
+    const link = svg
+      .selectAll(".lines")
+      .selectAll("line")
+      .data(data.links)
+      .join("line")
+      .style("stroke", "#aaa");
+
+    // Initialize the nodes
+    const node = svg
+      .selectAll(".nodes")
+      .selectAll("circle")
+      .data(data.nodes)
+      .join((enter) => {
+        const g = enter.append("g");
+
+        g.append("circle")
+          .attr("r", (d) => {
+            const links = data.links.filter((link) => link.source === d.id);
+            return CIRCLE_BASE_RADIUS + links.length * CIRCLE_RADIUS_MULTIPLIER;
+          })
+          .style("fill", (d) => d.color || "#69b3a2")
+          .call((e) => e.transition());
+
+        g.append("text")
+          .text((d) => d.name)
+          .join("text")
+          .attr("text-anchor", "middle");
+
+        return g;
+      });
+
+    function ticked() {
+      link
+        .attr("x1", function (d) {
+          return d.source.x;
+        })
+        .attr("y1", function (d) {
+          return d.source.y;
+        })
+        .attr("x2", function (d) {
+          return d.target.x;
+        })
+        .attr("y2", function (d) {
+          return d.target.y;
+        });
+
+      node
+        .selectAll("circle")
+        .attr("cx", function (d) {
+          return d.x + 6;
+        })
+        .attr("cy", function (d) {
+          return d.y - 6;
+        });
+
+      node
+        .selectAll("text")
+        .attr("x", function (d) {
+          return d.x + 6;
+        })
+        .attr("y", function (d) {
+          return d.y;
+        });
+    }
+  }, [data.links, data.nodes]);
+
+  useEffect(() => draw(), [draw]);
 
   return (
-    <SNetworkContainer>
+    <div>
       This is a graph
       <div>
         <svg ref={graphRef}></svg>
       </div>
-    </SNetworkContainer>
+    </div>
   );
 }
 
